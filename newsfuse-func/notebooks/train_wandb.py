828fb3dafbf142798acf6de72c3258f8062f7dcc
AndrzejKaj <andrzej.kajdasz1@gmail.com>
2023-06-10 13:00:36 +0200

    Add wandb Trainer

---
+++ newsfuse-func/notebooks/train_wandb.py
@@ -0,0 +1,87 @@
+import wandb
+import os
+from official.nlp import optimization
+import tensorflow as tf
+import nltk
+from lime.lime_text import LimeTextExplainer
+import numpy as np
+
+class Trainer():
+    def __init__(self, config, model, dataset) -> None:
+        self.config = config
+        self.model = model
+        self.dataset = dataset
+        self.get_dataset_partitions_tf(dataset, config["datasize"])
+        self.init_wandb()
+
+    def init_wandb(self):
+        wandb.init(
+            project="news-fuse",
+            config=self.config,
+            settings=wandb.Settings(code_dir="...")
+        )
+        wandb.log(self.config)
+
+        
+    def get_dataset_partitions_tf(self, ds, ds_size, train_split=0.9, val_split=0.05, test_split=0.05):
+        assert (train_split + test_split + val_split) == 1
+        
+        train_size = int(train_split * ds_size)
+        val_size = int(val_split * ds_size)
+        
+        self.train_ds = ds.take(train_size).batch(32)
+        self.val_ds = ds.skip(train_size).take(val_size).batch(32)
+        self.test_ds = ds.skip(train_size).skip(val_size).batch(32)
+        
+
+    def compile_model(self):
+        optimizer = optimization.create_optimizer(
+            init_lr=self.config["init_lr"],
+            num_train_steps=self.config["num_train_steps"],    
+            num_warmup_steps=self.config["num_warmup_steps"],
+            optimizer_type='adamw'
+        )
+        loss = tf.keras.losses.BinaryCrossentropy()
+        metrics = tf.metrics.BinaryAccuracy()
+
+        self.model.compile(
+            optimizer=optimizer,
+            loss=loss,  
+            metrics=metrics
+        )
+
+    def train(self):
+        history = self.model.fit(
+            x=self.train_ds,
+            epochs=self.config["epochs"],
+            validation_data=self.val_ds,
+            callbacks=[wandb.keras.WandbCallback(save_model = False)]
+        )
+
+    def test_model(self):
+        test_data = self.model.evaluate(self.test_ds)
+        wandb.log({"test loss": test_data[0],
+                   "test binary_accuracy": test_data[1]})
+        return test_data
+    
+    def predict_model(self):
+        return self.model.predict(self.test_ds,
+            callbacks=[wandb.keras.WandbCallback()])
+
+    def save_model(self):
+        self.model.save(os.path.join(wandb.run.dir, "model.h5"))
+
+    def get_probabilities(self, text):
+        prob = self.model.predict([text])
+        return np.hstack([1 - prob, prob])
+
+    def explain_prediction(self, text, log = True):
+        explainer = LimeTextExplainer(class_names=["Non-biased", "Biased"])
+        with tf.device("/cpu:0"):
+            explanation = explainer.explain_instance(text, self.get_probabilities, num_features=10)
+            if log:
+                data = [[label, val] for (label, val) in explanation.as_list()]
+                table = wandb.Table(data=data, columns = ["label", "value"])
+                wandb.log({"my_bar_chart_id" : wandb.plot.bar(table, "label", "value", title="Custom Bar Chart")})
+            return explanation
+
